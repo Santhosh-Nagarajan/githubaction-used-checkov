fe0a0ed69fe290c52d1d8cc579b42b3a5379ee90
Santhosh-Nagarajan <103426295+Santhosh-Nagarajan@users.noreply.github.com>
2023-09-26 11:52:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,72 +3,53 @@ resource "google_compute_instance" "default" {
   machine_type = var.machine-type
   zone         = var.zone
 
-
   tags = ["foo", "bar"]
 
   boot_disk {
     initialize_params {
-      size   = var.size
-      image  = var.image-name
-      labels = var.labels
+      size  = var.size
+      image = var.image-name
+      labels = {
+        my_label = "value"
+      }
     }
-    kms_key_self_link = "key-link"
-
-  }
-
-  shielded_instance_config {
-    enable_integrity_monitoring = true
-    enable_vtpm                 = true
-
   }
 
   network_interface {
-    network    = var.network
-    subnetwork = var.subnetwork
+    network    = "default"
+    access_config {}
   }
-
-  metadata = {
-    block-project-ssh-keys = true
-  }
-
-
-  metadata_startup_script = "echo hi > /test.txt"
+  metadata_startup_script = var.startup-script
 
   service_account {
-    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
-    email  = "[PROJECT_NUMBER]-compute@developer.gserviceaccount.com"
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = var.email
+    scopes = ["cloud-platform"]
   }
-
-
-
-
-
-
-
 }
 
 # Create HTTP firewall rule
 resource "google_compute_firewall" "http_firewall" {
-  name    = "http-firewall"
-  network = google_compute_instance.default.network_interface
+  name    = google_compute_instance.default.name
+  network = "default"
 
   allow {
     protocol = "tcp"
-    ports    = ["80",,443"]
+    ports    = ["80", "443"]
   }
 
-  source_ranges = var.firewal-source-range
+  source_ranges = ["0.0.0.0/0"]
 }
 
 # Create SSH firewall rule
 resource "google_compute_firewall" "ssh_firewall" {
-  name    = "ssh-firewall"
-  network = google_compute_instance.default.id
+  name    = "firewallname1"
+  network = "default"
 
   allow {
     protocol = "tcp"
-    ports    = ["22, ${var.port}"]
+    ports    = ["22", "8080"]
   }
 
-  source_ranges = var.firewall-ssh-source-range
+  source_ranges = ["0.0.0.0/0"]
 }

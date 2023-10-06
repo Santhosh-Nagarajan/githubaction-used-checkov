75612928ea8d3a18e377aa108016ac09aaa0c40f
Santhosh-Nagarajan <103426295+Santhosh-Nagarajan@users.noreply.github.com>
2023-10-06 12:32:42 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,50 @@
+variable "project-id" {
+  type        = string
+  description = "Enter Your Project Id"
+}
+
+variable "region" {
+  type        = string
+  description = "Enter Your Region"
+}
+
+variable "zone" {
+  type        = string
+  description = "Enter Your zone"
+}
+
+variable "credential" {
+  type        = string
+  description = "Enter your JSON file"
+}
+
+variable "instance-name" {
+  type        = string
+  description = "Enter Your Instance Name"
+}
+
+variable "machine-type" {
+  type        = string
+  description = "Enter Your Instance Machine Type"
+}
+
+
+variable "size" {
+  type        = string
+  description = "Enter Your Size"
+}
+
+variable "image-name" {
+  type        = string
+  description = "Enter Your Image Name"
+}
+
+variable "startup-script" {
+  type        = string
+  description = "Enter Your Subnetwork"
+}
+
+variable "email" {
+  type        = string
+  description = "Enter Your email"
+}

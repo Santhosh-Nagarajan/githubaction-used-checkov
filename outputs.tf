180fa7f8fc8f7ead4903f4044b16fd56935d69be
Santhosh-Nagarajan <nsmgbros001@gmail.com>
2024-01-23 09:39:08 +0000

    create output file

--- outputs.tf
+++
@@ -1,6 +0,0 @@
-output "cloud-run-url" {
-  value = google_cloud_run_service.example_service.status[0].url
-}
-output "cloud-run-id" {
-  value = google_cloud_run_service.example_service.id
-}
--- provider.tf
+++
@@ -1,7 +0,0 @@
-
-provider "google" {
-  project     = var.project
-  region      = var.region
-  zone        = var.zone
-  credentials = file("./${var.credential}")
-}
